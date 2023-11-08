e246e51cf2517f82d4f56ffc84d9a45bcc648df3
gabsferrara <gabrielbarbosa_ferrara@hotmail.com>
2023-11-08 18:14:41 -0300

    feat: :sparkles: outputs

--- local.tf
+++ local.tf
@@ -7,3 +7,11 @@ variable "conteudo" {
   type = string
   default = "Hellow World"
 }
+
+output "id-file" {
+    value = resource.local_file.exemplo.id
+}
+
+output "conteudo" {
+    value = resource.local_file.exemplo.content
+}
